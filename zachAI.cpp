bace5a51a35084d0fbef9343d6ecd25636d10bc0
Dan <danny1996.2001@gmail.com>
2018-03-04 23:29:12 -0800

    Created the files for the AI.

---
+++ zachAI.cpp
@@ -0,0 +1,6 @@
+/**
+ * @file
+ *
+ * @brief Othello AI that plays as well as Zach does.
+ *
+ */
---
+++ zachAI.hpp
@@ -0,0 +1,6 @@
+/**
+ * @file
+ *
+ * @brief Header file for the Othello AI that plays as well as Zach does.
+ *
+ */
